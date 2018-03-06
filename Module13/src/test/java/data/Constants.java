bd628f760808f207b7110255dee9a7d11c49dab7
Lusine Hakobyan <lusine_hakobyan@epam.com>
2018-03-06 23:29:30 +0400

    Revert "minor fixes"
    This reverts commit be7986deeba4957a4db7008ae4b258dd063f3c02.

--- Module13/src/test/java/data/Constants.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-public class Constants {
-    public static final String URL = "https://accounts.google.com";
-    public static final String USERNAME = "testcaseforselenium";
-    public static final String PASSWORD = "9386206687";
-}
